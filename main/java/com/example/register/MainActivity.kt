6f3900ab8d6e8c617721a9dd3880522ebfd4520e
Gorshenina <eugorshenina@yandex.ru>
2020-05-22 14:02:21 +0300

    task

--- main/java/com/example/register/MainActivity.kt
+++ main/java/com/example/register/MainActivity.kt
@@ -2,14 +2,23 @@ package com.example.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.login.*
+import kotlinx.android.synthetic.main.user_registration.*
+import kotlinx.android.synthetic.main.user_registration.email
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var handler: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        handler = SharedPreferences( this)
 
         showHome()
 
@@ -20,6 +29,19 @@ class MainActivity : AppCompatActivity() {
         login.setOnClickListener {
             showLogin()
         }
+
+        save.setOnClickListener {
+            handler.insertUserData(имя.text.toString(), email.text.toString(), пароль.text.toString())
+            showHome()
+        }
+
+        войти.setOnClickListener {
+            if (handler.userPresent(email.text.toString(), пароль.text.toString()))
+                Toast.makeText(this, "Login success", Toast.LENGTH_SHORT).show()
+            else
+                Toast.makeText(this, "Username or password is incorrect", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     private fun showRegistration(){
--- main/java/com/example/register/SharedPreferences.kt
+++ main/java/com/example/register/SharedPreferences.kt
@@ -1,2 +1,47 @@
 package com.example.register
 
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class SharedPreferences (context : Context) : SQLiteOpenHelper (context, spname, factory, version) {
+
+    override fun onCreate(p0: SQLiteDatabase?) {
+        p0?.execSQL("create table user (id integer primary key autoincrement," +
+        "name varchar(30), email varchar(100), passvord varchar(20)")
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun insertUserData (name: String, email: String, password: String){
+        val db: SQLiteDatabase = writableDatabase
+        val values: ContentValues = ContentValues()
+        values.put("name", name)
+        values.put("email", email)
+        values.put("password", password)
+
+        db.insert("user", null, values)
+        db.close()
+    }
+
+    fun userPresent (email: String, password: String): Boolean {
+        val db = writableDatabase
+        val query = "выбрать пользователя, email которого - $email, а пароль - $password"
+        val cursor = db.rawQuery(query, null)
+        if (cursor.count <= 0) {
+            cursor.close()
+            return false
+        }
+        cursor.close()
+        return true
+    }
+
+    companion object {
+        internal val spname = "userSP"
+        internal val factory = null
+        internal val version = 1
+    }
+}
